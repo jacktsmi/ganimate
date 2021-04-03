310dd7c4a47a15765afd795a65690dc70c680c66
jacktsmi <jacktsmi@umich.edu>
2021-04-03 19:21:54 -0400

    Fixed relu calls.

--- models.py
+++ models.py
@@ -6,9 +6,9 @@ class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
         # Encoder
-        self.enc1 = nn.InstanceNorm2d(nn.ReLU(nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7, stride=1)))
-        self.enc2 = nn.InstanceNorm2d(nn.ReLU(nn.Conv2d(in_channels=32, out_channels=128, kernel_size=3, stride=2)))
-        self.enc3 = nn.InstanceNorm2d(nn.ReLU(nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=2)))
+        self.enc1 = nn.InstanceNorm2d(F.relu(nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7, stride=1), inplace=True))
+        self.enc2 = nn.InstanceNorm2d(F.relu(nn.Conv2d(in_channels=32, out_channels=128, kernel_size=3, stride=2), inplace=True))
+        self.enc3 = nn.InstanceNorm2d(F.relu(nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=2), inplace=True))
         # Transformer (Residual Blocks)
         self.res1 = ResidualBlock()
         self.res2 = ResidualBlock()
@@ -17,9 +17,9 @@ class Generator(nn.Module):
         self.res5 = ResidualBlock()
         self.res6 = ResidualBlock()
         # Decoder
-        self.dec1 = nn.InstanceNorm2d(nn.ReLU(nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3, stride=2)))
-        self.dec2 = nn.InstanceNorm2d(nn.ReLU(nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, stride=2)))
-        self.dec3 = nn.InstanceNorm2d(nn.ReLU(nn.Conv2d(in_channels=64, out_channels=3, kernel_size=7, stride=1)))
+        self.dec1 = nn.InstanceNorm2d(F.relu(nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3, stride=2), inplace=True))
+        self.dec2 = nn.InstanceNorm2d(F.relu(nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, stride=2), inplace=True))
+        self.dec3 = nn.InstanceNorm2d(F.relu(nn.Conv2d(in_channels=64, out_channels=3, kernel_size=7, stride=1), inplace=True))
     
     def forward(self, x):
         out = self.enc1(x)
@@ -45,8 +45,8 @@ class ResidualBlock(nn.Module):
     
     def forward(self, x):
         residual = x
-        out1 = nn.InstanceNorm2d(nn.ReLU(self.conv1(x)))
-        out2 = nn.InstanceNorm2d(nn.ReLU(self.conv2(out1)))
+        out1 = nn.InstanceNorm2d(F.relu(self.conv1(x), inplace=True))
+        out2 = nn.InstanceNorm2d(F.relu(self.conv2(out1), inplace=True))
         out = out2 + residual
         out = out.view(out.size(0), -1)
 
